60e1b4fe85212f0537450f24ca1e0f109384945b
Ranjan Sahu <sahu.ranjanrn@gmail.com>
2023-06-04 22:41:59 +0530

    footer added

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,7 +13,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
-  <body>
+  <body style="min-height: 100vh;
+    display: flex;
+    flex-direction: column;">
       <div class="container-fluid bg-primary">
         <div class="container">
             <nav class="navbar navbar-expand-sm">
